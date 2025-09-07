93a149c38b13338ceb381d5b7cfb63ea0d3a3c6f
aayush <hiaayush30@gmail.com>
2025-09-07 23:36:14 +0530

    init

---
+++ prisma/migrations/20250907180056_init_schema/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."Todo" (
+    "id" SERIAL NOT NULL,
+    "todo" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
